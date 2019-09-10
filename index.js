ad1fcb424173ac85be418bdf7451aacf756bc17b
Thomas Kottke <t.kottke90@gmail.com>
2019-09-09 21:11:49 -0500

    feat: Add Socket.io

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 
 const PORT = process.env.PORT || 3000;
 
+// Configure App
+app.io = require('socket.io')(server);
 app.logger = require('./modules/logger');
 app.use(express.json());
 
+app.io.on('connection', socket => {
+	logger.info('Socket Connected', { ...socket });
+});
 
+// Main Route
+app.get('/', express.static('./public'));
 
+// Setup listening port
 app.listen(PORT, () => {
 	app.logger.info(`Express app started on port ${PORT}`, { port: PORT })
 });
