656e01b4cbbd1e620382f742275f8a7949405da4
Thomas Kottke <t.kottke90@gmail.com>
2019-09-09 21:11:35 -0500

    refactor: Update log location

--- modules/logger.js
+++ modules/logger.js
@@ -11,8 +11,8 @@ const logger = createLogger({
   ),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: '../logs/combined.log', level: 'info'}),
-    new transports.File({ filename: '../logs/error.log', level: 'error' })
+    new transports.File({ filename: './logs/combined.log', level: 'info'}),
+    new transports.File({ filename: './logs/error.log', level: 'error' })
   ],
 });
 
