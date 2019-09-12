1d4e216693db96a3b2ef0d1288179d483b7f9c6c
Thomas Kottke <t.kottke90@gmail.com>
2019-09-11 21:44:36 -0500

    feat: Add and implement showdown

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
+const http = require('http').createServer(app);
+const showdown = require('showdown');
+const converter = new showdown.Converter();
+
+converter.setFlavor('github')
 
 const PORT = process.env.PORT || 3000;
 
 // Configure App
-app.io = require('socket.io')(server);
+app.io = require('socket.io')(http);
 app.logger = require('./modules/logger');
 app.use(express.json());
 
+
+// Configure Socket.IO
+
 app.io.on('connection', socket => {
-	logger.info('Socket Connected', { ...socket });
+	app.logger.info('Socket Connected', { id: socket.id });
+
+	socket.on('convert markdown', params => {
+		socket.emit('convert markdown', { html: converter.makeHtml(params.markdown) });
+	});
+
 });
 
 // Main Route
 app.use('/', express.static('./public'));
 
+app.post('/test', (req, res) => {
+	res.send(converter.makeHtml(req.body.text));
+});
+
 // Setup listening port
-app.listen(PORT, () => {
+http.listen(PORT, () => {
 	app.logger.info(`Express app started on port ${PORT}`, { port: PORT })
 });
